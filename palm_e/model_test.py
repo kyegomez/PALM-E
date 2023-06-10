5ef4e03270500461677955afe2875e179e8decd1
Kye <kye@apacmediasolutions.com>
2023-06-09 22:27:42 -0400

    super deep error handling for everything

--- palm_e/model_test.py
+++ palm_e/model_test.py
@@ -7,22 +7,41 @@ text = "This is a sample text"
 
 # Load a sample image
 image_path = "galaxy-andromeda.jpeg"
-image = Image.open(image_path)
-transform = transforms.Compose([transforms.Resize((1024, 1024)), transforms.ToTensor()])
-image = transform(image)
-image = image.unsqueeze(0)  # Add batch dimension
+try:
+
+    image = Image.open(image_path)
+    transform = transforms.Compose([transforms.Resize((1024, 1024)), transforms.ToTensor()])
+    image = transform(image)
+    image = image.unsqueeze(0)  # Add batch dimension
+
+except Exception as e:
+    print(f"Error loading and processing image: {e}")
 
 # Instantiate tokenizer and tokenize inputs
-tokenizer = PALME_Tokenizer()
-tokenized_inputs = tokenizer.tokenize({"target_text": text, "image": image})
+try:
+        
+    tokenizer = PALME_Tokenizer()
+    tokenized_inputs = tokenizer.tokenize({"target_text": text, "image": image})
+except Exception as e:
+    print(f"Error tokenzing inputs: {e}")
 
-# Instantiate model
-model = PALME()
+try:
+        
+    # Instantiate model
+    model = PALME()
+except Exception as e:
+    print(f"Error initlizing model: {e}")
 
-# Call the forward function and print the output
-output = model.forward(
-    text_tokens=tokenized_inputs["text_tokens"],
-    images=tokenized_inputs["images"]
-)
+try:
+        
 
-print(f'output: {output}')
+    # Call the forward function and print the output
+    output = model.forward(
+        text_tokens=tokenized_inputs["text_tokens"],
+        images=tokenized_inputs["images"]
+    )
+
+    print(f'output: {output}')
+
+except Exception as e:
+    print(f"Output call pass: {e}")
