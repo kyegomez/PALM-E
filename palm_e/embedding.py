2e0998404e2a0938589a536ce8c336a60b52c7df
Kye <kye@apacmediasolutions.com>
2023-06-09 20:08:06 -0400

    perceive image

--- palm_e/embedding.py
+++ palm_e/embedding.py
@@ -28,7 +28,7 @@ class VisionEmbedding(nn.Module):
 
     def __init__(
             self,
-            img_size=1024,
+            img_size=224,
             patch_size=16,
             in_chans=3,
             embed_dim=768,
