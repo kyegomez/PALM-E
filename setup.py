005b689ea27ba94275c69a2a7e916974ebc26a1f
Kye <kye@apacmediasolutions.com>
2023-08-03 08:21:57 -0400

    verison

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'palme',
   packages = find_packages(exclude=['examples']),
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'palme - Pytorch',
   author = 'Kye Gomez',
