9192af69af39822a91c43dc25e39a1461ba85acf
Eternal Reclaimer <98760976+kyegomez@users.noreply.github.com>
2023-12-24 18:24:32 -0500

    Update model.py

--- palme/model.py
+++ palme/model.py
@@ -1,8 +1,7 @@
 import torch
-import torch.nn as nn
-from transformers import AutoTokenizer, CLIPProcessor
-
-from palme.transformer import (
+from torch import nn
+from zeta.structs import (
+    AutoregressiveWrapper,
     Decoder,
     Encoder,
     Transformer,
@@ -10,89 +9,52 @@ from palme.transformer import (
 )
 
 
-class PalmeETokenizer:
-    def __init__(self):
-        try:
-            self.processor = CLIPProcessor.from_pretrained(
-                "laion/CLIP-ViT-L-14-laion2B-s32B-b82K"
-            )
-            self.tokenizer = AutoTokenizer.from_pretrained(
-                "EleutherAI/gpt-neox-20b",
-                additional_special_tokens=["<image>", "</image>"],
-                eos_token="<eos>",
-                pad_token="<pad>",
-                extra_ids=0,
-                model_max_length=8192,
-            )
+class PalmE(nn.Module):
+    """
+    RT2 model implementation.
 
-            self.im_idx, self.im_end_idx = self.tokenizer.convert_tokens_to_ids(
-                ["<image>", "</image>"]
-            )
-        except Exception as e:
-            print(f"Error init tokenizer: {e}")
+    Args:
+        image_size (int): Size of the input image.
+        patch_size (int): Size of each image patch.
+        encoder_dim (int): Dimension of the encoder.
+        encoder_depth (int): Depth of the encoder.
+        encoder_heads (int): Number of attention heads in the encoder.
+        num_tokens (int): Number of tokens in the decoder.
+        max_seq_len (int): Maximum sequence length in the decoder.
+        decoder_dim (int): Dimension of the decoder.
+        decoder_depth (int): Depth of the decoder.
+        decoder_heads (int): Number of attention heads in the decoder.
+        attn_kv_heads (int): Number of attention heads for key-value projection.
+        use_abs_pos_emb (bool): Whether to use absolute positional embeddings.
+        cross_attend (bool): Whether to enable cross-attention in the decoder.
+        attn_flash (bool): Whether to enable flash attention in the decoder.
+        qk_norm (bool): Whether to normalize queries and keys in attention.
 
-    def tokenize_texts(self, texts):
-        try:
-            texts = self.tokenizer(
-                texts, return_tensors="pt", padding=True, truncation=True
-            ).input_ids
-            image_tokens = torch.tensor(
-                [[self.im_idx, self.im_end_idx]] * texts.shape[0]
-            )
-            return torch.cat([texts[:, 0:1], image_tokens, texts[:, 1:]], dim=1), texts
-        except Exception as e:
-            print(f"Error tokenizing texts: {e}")
+    Attributes:
+        encoder (ViTransformerWrapper): Encoder module.
+        decoder (AutoregressiveWrapper): Decoder module.
 
-    def tokenize_images(self, images):
-        try:
-            tokenized_images = self.processor(
-                images=images, return_tensors="pt"
-            ).pixel_values
-            print(f"Tokenized image: {tokenized_images.shape}")
-            return tokenized_images
+    """
 
-        except Exception as e:
-            print(f"Error tokenizing texts: {e}")
-
-    def tokenize(self, sample):
-        try:
-            text_tokens, only_text_tokens = self.tokenize_texts(sample["target_text"])
-            attention_mask = text_tokens != self.tokenizer.pad_token_id
-            dummy_image_features = torch.ones((text_tokens.shape[0], 64))
-            attention_mask = torch.cat([dummy_image_features, attention_mask], dim=1)
-            return {
-                "text_tokens": text_tokens,
-                "images": self.tokenize_images(sample["image"]),
-                "labels": only_text_tokens,
-                "attention_mask": attention_mask,
-            }
-
-        except Exception as e:
-            print(f"Error during tokenization {e}")
-
-
-class PalmE(torch.nn.Module):
     def __init__(
         self,
-        image_size=256,
-        patch_size=32,
-        encoder_dim=512,
-        encoder_depth=6,
-        encoder_heads=8,
-        num_tokens=20000,
-        max_seq_len=1024,
-        decoder_dim=512,
-        decoder_depth=6,
-        decoder_heads=8,
-        alibi_num_heads=4,
-        use_abs_pos_emb=False,
-        cross_attend=True,
-        alibi_pos_bias=True,
-        rotary_xpos=True,
-        attn_flash=True,
-        qk_norm=True,
+        image_size: int = 256,
+        patch_size: int = 32,
+        encoder_dim: int = 512,
+        encoder_depth: int = 6,
+        encoder_heads: int = 8,
+        num_tokens: int = 20000,
+        max_seq_len: int = 1024,
+        decoder_dim: int = 512,
+        decoder_depth: int = 6,
+        decoder_heads: int = 8,
+        attn_kv_heads: int = 2,
+        use_abs_pos_emb: bool = False,
+        cross_attend: bool = True,
+        attn_flash: bool = True,
+        qk_norm: bool = True,
     ):
-        super(PalmE, self).__init__()
+        super(RT2, self).__init__()
 
         self.encoder = ViTransformerWrapper(
             image_size=image_size,
@@ -111,15 +73,29 @@ class PalmE(torch.nn.Module):
                 depth=decoder_depth,
                 heads=decoder_heads,
                 cross_attend=cross_attend,
-                alibi_pos_bias=alibi_pos_bias,
-                alibi_num_heads=alibi_num_heads,
-                rotary_xpos=rotary_xpos,
+                attn_kv_heads=attn_kv_heads,
                 attn_flash=attn_flash,
                 qk_norm=qk_norm,
             ),
         )
 
-    def forward(self, img, text):
+        self.decoder = AutoregressiveWrapper(self.decoder)
+
+    def forward(self, img: torch.Tensor, text: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the RT2 model.
+
+        Args:
+            img (torch.Tensor): Input image tensor.
+            text (torch.Tensor): Input text tensor.
+
+        Returns:
+            torch.Tensor: Output tensor.
+
+        Raises:
+            Exception: If an error occurs during the forward pass.
+
+        """
         try:
             encoded = self.encoder(img, return_embeddings=True)
             return self.decoder(text, context=encoded)
